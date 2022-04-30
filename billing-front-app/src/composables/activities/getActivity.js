c52f782e440c282880179d59c07bc1a545e401c9
EstevanGAY <estevan.gay@gmail.com>
2022-05-01 00:35:35 +0200

    Add Edit activity page + composables for activities and bills (empty)

---
+++ billing-front-app/src/composables/activities/getActivity.js
@@ -0,0 +1,22 @@
+import { ref } from "vue";
+
+const getActivity = (id) => {
+  const activity = ref(null);
+  const error = ref(null);
+
+  const load = async () => {
+    try {
+      let data = await fetch("http://localhost:4000/activities/" + id);
+      if (!data.ok) {
+        throw Error("this activity doesn't exists");
+      }
+      activity.value = await data.json();
+    } catch (err) {
+      error.value = err.message;
+    }
+  };
+
+  return { activity, load };
+};
+
+export default getActivity;
